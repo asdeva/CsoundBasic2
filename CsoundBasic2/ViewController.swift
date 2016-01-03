5806ba8dd7017bd3c22cd93ebc778fd9c1243aab
cmillan <carlos@asdeva.com>
2016-01-03 14:26:50 -0800

    Initial Commit

---
+++ CsoundBasic2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CsoundBasic2
+//
+//  Created by Carlos Millan on 1/3/16.
+//  Copyright © 2016 Carlos Millan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
