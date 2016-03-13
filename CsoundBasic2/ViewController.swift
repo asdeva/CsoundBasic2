91f689127828ed07c0eee9d9e06541328709b7ce
cmillan <carlos@asdeva.com>
2016-03-12 18:04:30 -0800

    Oboe instrument

--- CsoundBasic2/ViewController.swift
+++ CsoundBasic2/ViewController.swift
@@ -23,16 +23,32 @@ class ViewController: UIViewController
     
     var lastUp: UIButton? = nil
     
-    var extendedNotes : [BasicInstrument.BasicNote] = []
+    typealias BasicNote = BasicInstrument.BasicNote
     
-    var buttonToNote : [UIButton: BasicInstrument.BasicNote] = [:]
+    var extendedNotes : [BasicNote] = []
+    
+    var buttonToNote : [UIButton: BasicNote] = [:]
+    
+    var numNotesOn = 0
+    
+    let melody = [5,5,8,5,5,8,5,8,13,12,10,10,8,3,5,6,3,3,5,6,3,6,12,10,8,12,13,
+    1,1,13,10,6,8,5,1,6,8,10,8,1,1,13,10,6,8,5,1,6,8,6,5,3,1]
+    
+    var i = 0
+    
+    var offset = 12
+    
+    var playBack = true
     
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         csound = CsoundObj()
         
-        instrument = BasicInstrument(csd: "flute", csound: csound)
+        //instrument = BasicInstrument(csd: "flute", csound: csound)
+        instrument = BasicInstrument(csd: "oboe1", csound: csound)
+        //instrument = BasicInstrument(csd: "oboe2", csound: csound)
+        //instrument = BasicInstrument(csd: "csscript", csound: csound)
         assignNoteToButton()
         view.multipleTouchEnabled = true
     }
@@ -57,34 +73,48 @@ class ViewController: UIViewController
         }
     }
     
+    
+    
     @IBAction func touchUp(sender: UIButton) {
         if let nu = buttonToNote[sender] {
-            if legato && nu.sounding {
+            if legato && nu.sounding  {
                 extendedNotes.append(nu)
-            } else {
+            } else if numNotesOn > 0 {
                 nu.sounding = false
+                --numNotesOn
             }
         }
     }
     
     
     @IBAction func noteKeyPressed(sender: UIButton) {
-        if let nd = buttonToNote[sender] {
+        if var nd = buttonToNote[sender] {
             if let ne = extendedNotes.first where legato {
                 extendedNotes.removeFirst()
                 if ne !== nd {
-                    NSTimer.schedule(delay: 0.15) {_ in
+                    NSTimer.schedule(delay: 0.01) {_ in
                         ne.sounding = false
+                        --self.numNotesOn
                     }
                     nd.volume = 0.5
+                    if playBack {
+                        nd.hertz = (baseNote, offset + melody[i])
+                        i = (i+1) % melody.count
+                    }
                     nd.sounding = true
+                    ++numNotesOn
                 } else {
                     ne.sounding = false
+                    --numNotesOn
                 }
-                
             } else {
-                nd.volume = 0.5
+                nd.volume = 0.3
+                if playBack {
+                    nd.hertz = (baseNote, offset + melody[i])
+                    i = (i+1) % melody.count
+                }
                 nd.sounding = true
+                ++numNotesOn
             }
         }
     }
